b1d41d46b557fa263ecebbfdf9af5048e0f64c72
AMIT-BHAGAT <bhagatamit94@gmail.com>
2024-02-14 14:43:45 +0530

    backend changess

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -16,7 +16,7 @@ terraform {
 
   backend "s3" {
 
-    bucket = "advanced-end-to-end-devsecops"
+    bucket = "kubernetesdevopsdata"
 
     key = "state/terraform.tfstate"
 
@@ -24,7 +24,7 @@ terraform {
 
     encrypt = true
 
-    dynamodb_table = "Terraform_lock"
+    dynamodb_table = "Kubernetes_devops"
 
   }
 
